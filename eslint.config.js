d6e17e0d495782a7109d51a02e818ad3dc634cc9
Justin Steines <30095726+justinsteines@users.noreply.github.com>
2024-04-06 11:26:58 -0500

    chore: add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
 import antfu from '@antfu/eslint-config'
 import tailwindcss from 'eslint-plugin-tailwindcss'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(antfu({
-  formatters: true,
-  vue: true,
-  ...{
-    ...tailwindcss.configs.recommended,
-    plugins: {
-      tailwindcss,
+export default withNuxt(
+  antfu({
+    vue: true,
+    stylistic: false,
+    formatters: true,
+    ...{
+      ...tailwindcss.configs.recommended,
+      plugins: {
+        tailwindcss,
+      },
     },
-  },
-}))
+  }),
+  eslintConfigPrettier
+)
